aafe04899abbe20fa78230adc54566896ce9f451
ahbucknell <128325035+ahbucknell@users.noreply.github.com>
2024-08-08 11:04:54 +0100

    add multichain PAE plot function

--- src/AlphaFoldMetrics.jl
+++ src/AlphaFoldMetrics.jl
@@ -17,7 +17,7 @@ function paeFromJSON(path::String)::Matrix{Float64}
     return jsonMx
 end
 
-function paeSoloChain(path::String)
+function makeSingleChainPAE(path::String)
     validateJSON(path)
     mx = paeFromJSON(path)
     arrlen = sqrt(length(mx))
@@ -26,4 +26,21 @@ function paeSoloChain(path::String)
     CairoMakie.heatmap!(ax1, mx, colorrange = (0,30), colormap = CairoMakie.Reverse(:Greens))
     return fig
 end
+
+function makeMultiChainPAE(path::String)
+    validateJSON(path)
+    jsonData = JSON.parsefile(path)
+    mx = Base.stack(jsonData["pae"])
+    startpoints = Dict{String,Int64}()
+    for chain in jsonData["atom_chain_ids"]
+        startpoints[chain] = findfirst(x -> x == chain, jsonData["token_chain_ids"])
+    end
+    div = collect(values(startpoints))
+    push!(div, length(jsonData["token_chain_ids"]))
+    f = CairoMakie.Figure()
+    ax1 = CairoMakie.Axis(f[1,1], xticks = div, yticks = div, aspect = 1)
+    CairoMakie.heatmap!(ax1, mx, colorrange = (0,30), colormap = CairoMakie.Reverse(:Greens))
+    return f
+end
+
 end
