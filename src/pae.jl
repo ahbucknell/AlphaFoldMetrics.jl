a6adba5c30214fb3ff95181d3349bde79a838829
ahbucknell <128325035+ahbucknell@users.noreply.github.com>
2024-08-28 13:57:24 +0100

    Add hidedecorations!() option to function args

--- src/pae.jl
+++ src/pae.jl
@@ -1,12 +1,15 @@
 # Contains all functions relating to Predicted Aligned Error (PAE) plots 
-function makeSingleChainPAE(paeMx::Matrix{Float64}, figure)
+function makeSingleChainPAE(paeMx::Matrix{Float64}, figure, ticks)
     arrlen = sqrt(length(paeMx))
     ax1 = CairoMakie.Axis(figure[1,1], yreversed = true, aspect = 1, xticks = [1,arrlen], yticks = [1,arrlen])
+    if !(ticks)
+        CairoMakie.hidedecorations!(ax1)
+    end
     CairoMakie.heatmap!(ax1, paeMx, colorrange = (0,30), colormap = CairoMakie.Reverse(:Greens))
     return figure
 end
 
-function makeMultiChainPAE(paeMx::Matrix{Float64}, json::Dict{String, Any}, figure)
+function makeMultiChainPAE(paeMx::Matrix{Float64}, json::Dict{String, Any}, figure, ticks)
     startpoints = Dict{String,Int64}()
     for chain in json["atom_chain_ids"]
         startpoints[chain] = findfirst(x -> x == chain, json["token_chain_ids"])
@@ -14,19 +17,22 @@ function makeMultiChainPAE(paeMx::Matrix{Float64}, json::Dict{String, Any}, figu
     div = collect(values(startpoints))
     push!(div, length(json["token_chain_ids"]))
     ax1 = CairoMakie.Axis(figure[1,1], xticks = div, yticks = div, aspect = 1)
+    if !(ticks)
+        CairoMakie.hidedecorations!(ax1)
+    end
     CairoMakie.heatmap!(ax1, paeMx, colorrange = (0,30), colormap = CairoMakie.Reverse(:Greens))
     return figure
 end
 
-function plotPAE(path::String, figure)
+function plotPAE(path::String, figure; ticks::Bool=true)
     validateJSON(path)
     jsonData = JSON.parsefile(path)
     # Below flattens vector of vectors into 2D Matrix{Any}, then converted to Matrix{Float64}
     mx = convert(Matrix{Float64}, Base.stack(jsonData["pae"]))
     numChains = length(unique(jsonData["token_chain_ids"]))
     if numChains > 1
-        makeMultiChainPAE(mx, jsonData, figure)
+        makeMultiChainPAE(mx, jsonData, figure, ticks)
     else
-        makeSingleChainPAE(mx, figure)
+        makeSingleChainPAE(mx, figure, ticks)
     end
 end
